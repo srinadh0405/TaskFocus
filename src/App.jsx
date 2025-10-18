68c59f9afbd45874bcea49010f099402b653cfd8
srinadh0405 <srinadh.nallabothula@gmail.com>
2025-10-18 10:35:17 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,24 @@ export default function App() {
       setTasks(tasks.filter((_, i) => i != index));
   }
 
+  const clearTasks = () => {
+    setTasks([]);
+  }
+
   return(
-    <div>
-      <h1>Task Focus</h1>
-      <p>Our friendly TaskManager</p>
+    <div className="App">
+      <header>
+      <h1 className="title">Task Focus</h1>
+      <p className="tagline">Our friendly TaskManager</p>
+      </header>
       <Taskform addTask = {addTask}/>
       <TaskList tasks = {tasks} 
       updateTask = {updateTask}
       deleteTask = {deleteTask}/>
       <Progresstracker tasks = {tasks}/>
-      <button>Clear all tasks</button>
+
+      {tasks.length>0 && (<button onClick={clearTasks} className="clear-btn">Clear all tasks</button>)}
+      
     </div>
   )
 }
